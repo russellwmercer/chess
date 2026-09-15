224c4dca3ac00c4d4c7e6e26f8cb662b39d3ac80
russellwmercer <rmercer2@byu.edu>
2026-09-15 08:39:52 -0600

    Completed ChessBoard.java and part of ChessPiece.java.

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,6 @@
 package chess;
 
+import java.util.Arrays;
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -7,10 +8,13 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessBoard {
+        ChessPiece[][] squares = new ChessPiece[8][8];
+        /** Saying 8 in this case is an allocation size - not an index, that's why we don't do 7 and 7.
+         * It's like saying - give me space for 8 pieces (0-7).
+         */
 
-    public ChessBoard() {
-        
-    }
+        public ChessBoard() {
+        }
 
     /**
      * Adds a chess piece to the chessboard
@@ -19,7 +23,7 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        squares[position.getRow()-1][position.getColumn()-1] = piece;
     }
 
     /**
@@ -30,14 +34,99 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        return squares[position.getRow()-1][position.getColumn()-1];
     }
-
     /**
      * Sets the board to the default starting board
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        //Rather than setting each middle position to null, it is more effective to just reset the board.
+        //I originally tried to create a new ChessBoard[] - but that would create a whole new object, rather than the change the one I'm editing.
+        //This sets the pointer for squares to a new array of pieces.
+        this.squares = new ChessPiece[8][8];
+        //Build the white team:
+        //Rooks:
+        addPiece((new ChessPosition(1,1)), (new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK) ));
+        addPiece((new ChessPosition(1,8)), (new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK) ));
+        //Knights:
+        addPiece((new ChessPosition(1,2)), (new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT) ));
+        addPiece((new ChessPosition(1,7)), (new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT) ));
+        //Bishops:
+        addPiece((new ChessPosition(1,3)), (new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP) ));
+        addPiece((new ChessPosition(1,6)), (new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP) ));
+        //King:
+        addPiece((new ChessPosition(1,5)), (new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KING) ));
+        //Queen:
+        addPiece((new ChessPosition(1,4)), (new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.QUEEN) ));
+        //Pawns:
+        for (int i = 1; i <= 8; i++) {
+            addPiece((new ChessPosition(2,i)), (new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN) ));
+        }
+
+        //Build the black team:
+        //Rooks:
+        addPiece((new ChessPosition(8,1)), (new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK) ));
+        addPiece((new ChessPosition(8,8)), (new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK) ));
+        //Knights:
+        addPiece((new ChessPosition(8,2)), (new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KNIGHT) ));
+        addPiece((new ChessPosition(8,7)), (new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KNIGHT) ));
+        //Bishops:
+        addPiece((new ChessPosition(8,3)), (new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP) ));
+        addPiece((new ChessPosition(8,6)), (new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP) ));
+        //King:
+        addPiece((new ChessPosition(8,5)), (new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KING) ));
+        //Queen:
+        addPiece((new ChessPosition(8,4)), (new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.QUEEN) ));
+        //Pawns:
+        for (int i = 1; i <= 8; i++) {
+            addPiece((new ChessPosition(7, i)), (new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN)));
+        }
+    }
+
+    /**
+     * @return a string that is an accurate representation of the chess board.
+     * Uses a StringBuilder object, which is helpful because you can use .append() and .toString() when finished.
+     * Iterate through all of the rows and columns, with letters for each string (from getPieceLetter) and space for blanks.
+     */
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i=7; i >=0; i--) {
+            for (int j=0; j <= 7; j++) {
+                if (squares[i][j] == null) {
+                    sb.append(" ");
+                }
+                else {
+                    sb.append(squares[i][j].getPieceLetter());
+                }
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+    /**
+     *
+     * @param o - another chess board to compare.
+     * @return boolean on if they are equal.
+     * Uses deepEquals, which iterates through each row and possible position.
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) { return true;}
+        if (o == null || getClass() != o.getClass()) {return false;}
+        ChessBoard other = (ChessBoard) o;
+        //deepEquals is in java.util.Arrays - iterates through all of the parts of them to see if they're accurate.
+        return Arrays.deepEquals(this.squares, other.squares);
+    }
+
+    /**
+     * @return integer hashcode.
+     * Arrays also has a deepHashCode function, which iterates through NESTED arrays. - a 2d board.
+     */
+    @Override
+    public int hashCode() {
+        return Arrays.deepHashCode(this.squares);
     }
 }
--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,6 +1,7 @@
 package chess;
 
 import java.util.Collection;
+import java.util.Objects;
 
 /**
  * Represents a single chess piece
@@ -9,8 +10,13 @@ import java.util.Collection;
  * signature of the existing methods.
  */
 public class ChessPiece {
+    private final ChessGame.TeamColor pieceColor;
+    private final ChessPiece.PieceType pieceType;
 
+    //Constructor
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.pieceColor = pieceColor;
+        this.pieceType = type;
     }
 
     /**
@@ -29,14 +35,14 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return pieceColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return pieceType;
     }
 
     /**
@@ -48,5 +54,49 @@ public class ChessPiece {
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {return true;}
+        if (o == null || this.getClass() != o.getClass()) {return false;}
+        ChessPiece other = (ChessPiece) o; // This line tells the compiler that the object passed into the function in this case is a ChessPiece.
+        return (other.getPieceType() == this.getPieceType() && other.getTeamColor() == this.getTeamColor()); // Make sure not to forget one of the object properties.
+    }
+
+    /**
+     * Uses Objects built in .hash() function to create a hashCode for the pieceColor and pieceType.
+     * @return an int hashcode.
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(pieceColor, pieceType);
+    }
+
+    /**
+     * Used by ChessBoard.java for generating strings of what the board looks like.
+     * @return a letter that signifies the pieceType and pieceColor.
+     */
+    public String getPieceLetter() {
+        if (pieceColor == ChessGame.TeamColor.WHITE) {
+            return switch (pieceType) {
+                case KING -> "K";
+                case QUEEN -> "Q";
+                case ROOK -> "R";
+                case KNIGHT -> "N";
+                case BISHOP -> "B";
+                case PAWN -> "P";
+            };
+        }
+        else {
+            return switch (pieceType) {
+                case KING -> "k";
+                case QUEEN -> "q";
+                case ROOK -> "r";
+                case KNIGHT -> "n";
+                case BISHOP -> "b";
+                case PAWN -> "p";
+            };
+        }
     }
 }
